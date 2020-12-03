eb61b0d66305091b5030bf1b9aca7db9aed1e6d8
ahmose231 <36313066+ahmose231@users.noreply.github.com>
2020-12-03 12:27:17 +0300

    Create 3b.c

---
+++ 3b.c
@@ -0,0 +1,42 @@
+// doesn't work
+
+#include <stdio.h>
+#define LINELEN 128
+
+int runslope(int right, int down, char filename[32])
+{
+	char line[LINELEN];
+	int path=0;
+	FILE *fp;
+	fp=fopen(filename,"r");
+	int len;
+	fgets(line,LINELEN,fp);
+	for(len=0;;len++)
+		if(line[len]=='\0')
+			break;
+	len--;
+	int treecount=0;
+	while(fgets(line,LINELEN,fp)!=NULL)
+	{
+		path=(path+right)%len;
+		if(line[path]=='#')
+			treecount++;
+		for(int i=1;i<down;i++)
+			fgets(line,LINELEN,fp);
+	}
+	fclose(fp);
+	printf("%d\n",treecount);
+	return treecount;
+}
+
+int main(int argc, char *argv[])
+{
+	long long totalcount=1;
+	
+	for(int i=1;i<8;i=i+2)
+		totalcount = totalcount * runslope(i,1,argv[1]);
+	totalcount = totalcount *  runslope(1,2,argv[1]);
+
+	printf("%lld\n",totalcount);
+	return 0;
+}

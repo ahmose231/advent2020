a6bb2be36b616768569d89d21b748a7faf52898c
ahmose231 <36313066+ahmose231@users.noreply.github.com>
2020-12-07 10:42:38 +0300

    Create 6b.c

---
+++ 6b.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LEN 32
+
+int main(int argc, char *argv[])
+{
+	char line[LEN];
+	FILE *fp=fopen(argv[1],"r");
+	
+	int alphabet[26];
+	int flag=0;
+	int line_len;
+	int sum;
+	int bigsum=0;
+	int p_count;
+	
+	while(1)
+	{
+		for(int i=0;i<26;i++)
+			alphabet[i]=0;
+		
+		p_count=0;
+		
+		while(1)
+		{
+			if(fgets(line,LEN,fp)==NULL)
+			{
+				flag=1;
+				break;
+			}
+			
+			if(line[0]=='\n')
+				break;
+			
+			p_count++;
+			
+			line_len=strlen(line);			
+			for(int i=0;i<line_len;i++)
+				alphabet[line[i]-'a']++;
+			
+		}
+		
+		sum=0;
+		
+		for(int i=0;i<26;i++)
+			if(alphabet[i]==p_count)
+				sum++;
+		
+		bigsum += sum;
+		
+		if(flag)
+			break;
+	}
+	
+	printf("%d\n",bigsum);
+	
+	return 0;
+}
